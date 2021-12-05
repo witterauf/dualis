73b43b57f4637b00485b303dd9271cb32e61c02f
Michael Witterauf <44549149+witterauf@users.noreply.github.com>
2021-12-05 15:07:53 +0100

    Refactor dualis-bmp slightly

--- examples/bmp/main.cc
+++ examples/bmp/main.cc
@@ -1,7 +1,58 @@
 #include <dualis.h>
+#include <iomanip>
 #include <iostream>
 
 using namespace dualis;
+
+enum class BitmapCompression : uint32_t
+{
+    RGB = 0,
+    RLE4 = 1,
+    RLE8 = 2,
+    BitFields = 3,
+};
+
+struct BitmapInfoHeader
+{
+    uint32_t biSize{0};
+    int32_t biWidth{0}, biHeight{0};
+    uint16_t biPlanes{1};
+    uint16_t biBitCount{32};
+    BitmapCompression biCompression{BitmapCompression::RGB};
+    uint32_t biImageSize{0};
+    int32_t biXPelsPerMeter{0}, biYPelsPerMeter{0};
+    uint32_t biClrUsed{0};
+    uint32_t biClrImportant{0};
+
+    bool isIndexed() const
+    {
+        return biBitCount <= 8;
+    }
+
+    auto paletteSize() const -> std::size_t
+    {
+        if (isIndexed())
+        {
+            return static_cast<std::size_t>(biClrUsed == 0 ? 1 << biBitCount : biClrUsed);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
+
+static_assert(sizeof(BitmapInfoHeader) == 40);
+
+void checkAgainstRawRead(byte_span bmp, const BitmapInfoHeader& infoHeader)
+{
+    // Only works on little-endian machines! Avoid if possible.
+    auto const infoHeaderB = read_raw<BitmapInfoHeader>(bmp, 14);
+    if (std::memcmp(&infoHeader, &infoHeaderB, sizeof(BitmapInfoHeader)) != 0)
+    {
+        std::cerr << "Warning: read mismatch\n";
+    }
+}
 
 auto readHeader(dualis::byte_span bmp) -> std::pair<size_t, size_t>
 {
@@ -11,21 +62,6 @@ auto readHeader(dualis::byte_span bmp) -> std::pair<size_t, size_t>
     return std::make_pair(size, offset);
 }
 
-struct BitmapInfoHeader
-{
-    uint32_t biSize{0};
-    int32_t biWidth{0}, biHeight{0};
-    uint16_t biPlanes{1};
-    uint16_t biBitCount{32};
-    uint32_t biCompression{0};
-    uint32_t biImageSize{0};
-    int32_t biXPelsPerMeter{0}, biYPelsPerMeter{0};
-    uint32_t biClrUsed{0};
-    uint32_t biClrImportant{0};
-};
-
-static_assert(sizeof(BitmapInfoHeader) == 40);
-
 auto readInfoHeader(byte_reader& reader) -> BitmapInfoHeader
 {
     BitmapInfoHeader infoHeader;
@@ -34,29 +70,72 @@ auto readInfoHeader(byte_reader& reader) -> BitmapInfoHeader
     infoHeader.biHeight = reader.consume_integer<int32_t, little_endian>();
     infoHeader.biPlanes = reader.consume_integer<uint16_t, little_endian>();
     infoHeader.biBitCount = reader.consume_integer<uint16_t, little_endian>();
-    infoHeader.biCompression = reader.consume_integer<uint32_t, little_endian>();
+    infoHeader.biCompression =
+        static_cast<BitmapCompression>(reader.consume_integer<uint32_t, little_endian>());
     infoHeader.biImageSize = reader.consume_integer<uint32_t, little_endian>();
     infoHeader.biXPelsPerMeter = reader.consume_integer<int32_t, little_endian>();
     infoHeader.biYPelsPerMeter = reader.consume_integer<int32_t, little_endian>();
     infoHeader.biClrUsed = reader.consume_integer<uint32_t, little_endian>();
     infoHeader.biClrImportant = reader.consume_integer<uint32_t, little_endian>();
+    checkAgainstRawRead(reader.span(), infoHeader);
     return infoHeader;
+}
+
+auto readPalette(byte_reader& reader, const BitmapInfoHeader& infoHeader) -> std::vector<uint32_t>
+{
+    std::vector<uint32_t> palette;
+    if (infoHeader.paletteSize() > 0)
+    {
+        palette.resize(infoHeader.paletteSize());
+        reader.consume_integer_sequence<uint32_t, little_endian>(palette.begin(),
+                                                                 infoHeader.paletteSize());
+    }
+    return palette;
 }
 
 void printInfoHeader(const BitmapInfoHeader& infoHeader)
 {
-    std::cout << "Dimensions: " << std::abs(infoHeader.biWidth) << "x"
-              << std::abs(infoHeader.biHeight) << " @ " << infoHeader.biBitCount << "bpp\n";
+    std::cout << "This bitmap is " << std::abs(infoHeader.biWidth) << "x"
+              << std::abs(infoHeader.biHeight) << " @ " << infoHeader.biBitCount << "bpp.\n";
+    std::size_t size{infoHeader.biImageSize};
+    switch (infoHeader.biCompression)
+    {
+    case BitmapCompression::RGB:
+        std::cout << "It is not compressed";
+        if (size == 0)
+        {
+            size = infoHeader.biWidth * infoHeader.biHeight * infoHeader.biBitCount / 8;
+        }
+        break;
+    case BitmapCompression::RLE4: std::cout << "It is compressed using RLE4"; break;
+    case BitmapCompression::RLE8: std::cout << "It is compressed using RLE8"; break;
+    case BitmapCompression::BitFields: std::cout << "It is compressed using BitFields"; break;
+    default: std::cout << "It is using an unknown or invalid compression"; break;
+    }
+    std::cout << ", taking up " << size << " bytes.\n";
 }
 
 void printPalette(const std::vector<uint32_t>& palette)
 {
     if (!palette.empty())
     {
-        std::cout << "This is an indexed bitmap:\n";
-        for (unsigned i = 0; i < palette.size(); ++i)
+        std::cout << "It is indexed with the following palette:\n";
+        for (unsigned i = 0; i < palette.size();)
         {
-            std::cout << " Color " << i << ": " << palette[i] << "\n";
+            std::cout << "  ";
+            for (unsigned j = 0; j < 8 && i < palette.size(); ++i, ++j)
+            {
+                if (j > 0)
+                {
+                    std::cout << " ";
+                }
+                std::cout << "[" << std::setw(3) << std::setfill(' ') << std::dec << i << "] ";
+                std::cout << "#" << std::setfill('0') << std::hex;
+                std::cout << std::setw(2) << ((palette[i] >> 16) & 0xff) << std::setw(2)
+                          << ((palette[i] >> 8) & 0xff) << std::setw(2)
+                          << ((palette[i] >> 0) & 0xff);
+            }
+            std::cout << "\n";
         }
     }
 }
@@ -75,28 +154,11 @@ void printBitmapInfo(const std::filesystem::path& path)
     }
 
     auto const [size, offset] = readHeader(bmpSpan);
-
     byte_reader reader{bmpSpan};
     reader.seekg(14);
-    auto const infoHeaderA = readInfoHeader(reader);
-    // Only works on little-endian machines! Avoid if possible.
-    auto const infoHeaderB = read_raw<BitmapInfoHeader>(bmpSpan, 14);
-
-    if (std::memcmp(&infoHeaderA, &infoHeaderB, sizeof(BitmapInfoHeader)) != 0)
-    {
-        std::cerr << "Warning: read mismatch\n";
-    }
-
-    std::vector<uint32_t> palette;
-    if (infoHeaderA.biBitCount <= 8)
-    {
-        auto const count =
-            infoHeaderA.biClrUsed == 0 ? (1 << infoHeaderA.biBitCount) : infoHeaderA.biClrUsed;
-        palette.resize(count);
-        reader.consume_integer_sequence<uint32_t, little_endian>(palette.begin(), count);
-    }
-
-    printInfoHeader(infoHeaderA);
+    auto const infoHeader = readInfoHeader(reader);
+    auto const palette = readPalette(reader, infoHeader);
+    printInfoHeader(infoHeader);
     printPalette(palette);
 }
 
