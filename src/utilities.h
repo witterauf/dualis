9489af3d00b577c3ebba93b5baf0995feb7d591c
Michael Witterauf <44549149+witterauf@users.noreply.github.com>
2023-02-05 17:36:01 +0100

    Implement base64 decoding

--- src/utilities.h
+++ src/utilities.h
@@ -238,6 +238,102 @@ template <byte_range Bytes> auto to_base64(const Bytes& bytes) -> std::string
     return base64;
 }
 
+namespace detail {
+
+inline auto check_base64(std::string_view base64) -> std::optional<size_t>
+{
+    if (base64.length() == 1)
+    {
+        // Only 0, 2, and 3 characters are valid sequences.
+        return std::nullopt;
+    }
+    size_t i{0};
+    for (; i < base64.length() && base64[i] != '='; ++i)
+    {
+        const char c = base64[i];
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') ||
+              c == '+' || c == '/'))
+        {
+            return std::nullopt;
+        }
+    }
+    auto const length = i;
+    size_t padding{0};
+    for (; i < base64.length() && base64[i] == '='; ++i, ++padding)
+    {
+    }
+    if (i != base64.length() && padding > 2)
+    {
+        return std::nullopt;
+    }
+    return length;
+}
+
+} // namespace detail
+
+template <size_constructible_bytes Bytes>
+auto from_base64(std::string_view base64) -> std::optional<Bytes>
+{
+    static constexpr uint8_t Values[] = {
+        // clang-format off
+        // '+' = 0x2b
+        0x3e, 0x00, 0x00, 0x00, 0x3f,
+        // '0' = 0x30
+        0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b,
+        0x3c, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00,
+        // starting at 'A' = 0x41
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        0x18, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        // 'a' == 0x61
+        0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21,
+        0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29,
+        0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31,
+        0x32, 0x33,
+        // clang-format on
+    };
+
+    auto const maybeLength = detail::check_base64(base64);
+    if (!maybeLength)
+    {
+        return std::nullopt;
+    }
+    auto const bits = *maybeLength * 6;
+    auto const size = bits / 8;
+    Bytes bytes(size);
+    size_t i = 0, j = 0;
+
+    auto transform = [&bytes](size_t j, uint8_t value0, uint8_t value1, uint8_t value2,
+                              uint8_t value3) {
+        bytes[j + 0] = static_cast<std::byte>((value0 << 2) | (value1 >> 4));
+        bytes[j + 1] = static_cast<std::byte>((value1 << 4) | (value2 >> 2));
+        bytes[j + 2] = static_cast<std::byte>((value2 << 6) | value3);
+    };
+
+    for (; i + 4 <= base64.length(); i += 4, j += 3)
+    {
+        transform(j, Values[static_cast<size_t>(base64[i + 0]) - 0x2b],
+                  Values[static_cast<size_t>(base64[i + 1]) - 0x2b],
+                  Values[static_cast<size_t>(base64[i + 2]) - 0x2b],
+                  Values[static_cast<size_t>(base64[i + 3]) - 0x2b]);
+    }
+    if (i < base64.length())
+    {
+        auto const diff = base64.length() - i;
+        auto const value0 = Values[static_cast<size_t>(base64[i + 0]) - 0x2b];
+        auto const value1 = Values[static_cast<size_t>(base64[i + 1]) - 0x2b];
+        bytes[j + 0] = static_cast<std::byte>((value0 << 2) | (value1 >> 4));
+        if (diff == 1)
+        {
+            auto const value2 = Values[static_cast<size_t>(base64[i + 2]) - 0x2b];
+            bytes[j + 1] = static_cast<std::byte>((value1 << 4) | (value2 >> 2));
+        }
+    }
+    return bytes;
+}
+
 namespace literals {
 
 consteval auto operator"" _b(unsigned long long int value) -> std::byte
